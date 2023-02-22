5384228447f468d78571eea0ff1042f9a8ac5cae
Rishi Sundar <rishisundar.dev@gmail.com>
2023-02-22 16:56:13 +0530

    fix: handled exceptions

--- block-watch.js
+++ block-watch.js
@@ -24,104 +24,106 @@ const subscription = web3.eth.subscribe('logs', {
     filter
 }, (error, result) => {
     if (!error) {
-        if (result.blockNumber !== null) {
+        if (result && result.blockNumber !== null) {
+            try {
+                if (result.topics[0] === filter.topics[0]) {
+                    console.log('Transfer Log:', result);
 
-            // Decode the log data based on the event signature
-            if (result.topics[0] === filter.topics[0]) {
-                console.log('Result:', result);
+                    // Transfer event
+                    const data = web3.eth.abi.decodeLog(
+                        [{
+                                type: 'address',
+                                name: 'from',
+                                indexed: true
+                            },
+                            {
+                                type: 'address',
+                                name: 'to',
+                                indexed: true
+                            },
+                            {
+                                type: 'uint256',
+                                name: 'value'
+                            }
+                        ],
+                        result.data,
+                        result.topics.slice(1)
+                    );
+                    console.log(`Transfer from ${data.from} to ${data.to}: ${data.value} tokens.\n`);
+                } else if (result.topics[0] === filter.topics[1]) {
+                    console.log('Swap Log:', result);
 
-                // Transfer event
-                const data = web3.eth.abi.decodeLog(
-                    [{
-                            type: 'address',
-                            name: 'from',
-                            indexed: true
-                        },
-                        {
-                            type: 'address',
-                            name: 'to',
-                            indexed: true
-                        },
-                        {
-                            type: 'uint256',
-                            name: 'value'
-                        }
-                    ],
-                    result.data,
-                    result.topics.slice(1)
-                );
-                console.log(`Transfer from ${data.from} to ${data.to}: ${data.value} tokens`);
-            } else if (result.topics[0] === filter.topics[1]) {
-                console.log('Result:', result);
+                    // Swap event
+                    const data = web3.eth.abi.decodeLog(
+                        [{
+                                type: 'address',
+                                name: 'sender',
+                                indexed: true
+                            },
+                            {
+                                type: 'address',
+                                name: 'recipient',
+                                indexed: true
+                            },
+                            {
+                                type: 'uint256',
+                                name: 'amount0In'
+                            },
+                            {
+                                type: 'uint256',
+                                name: 'amount1In'
+                            }
+                        ],
+                        result.data,
+                        result.topics.slice(1)
+                    );
+                    console.log(`Swap from ${data.sender} to ${data.recipient}: ${data.amount0In} tokens of token0 and ${data.amount1In} tokens of token1.\n`);
+                } else if (result.topics[0] === filter.topics[2]) {
+                    console.log('Unstake Log:', result);
 
-                // Swap event
-                const data = web3.eth.abi.decodeLog(
-                    [{
-                            type: 'address',
-                            name: 'sender',
-                            indexed: true
-                        },
-                        {
-                            type: 'address',
-                            name: 'recipient',
-                            indexed: true
-                        },
-                        {
-                            type: 'uint256',
-                            name: 'amount0In'
-                        },
-                        {
-                            type: 'uint256',
-                            name: 'amount1In'
-                        }
-                    ],
-                    result.data,
-                    result.topics.slice(1)
-                );
-                console.log(`Swap from ${data.sender} to ${data.recipient}: ${data.amount0In} tokens of token0 and ${data.amount1In} tokens of token1`);
-            } else if (result.topics[0] === filter.topics[2]) {
-                console.log('Result:', result);
+                    // Unstake event
+                    const data = web3.eth.abi.decodeLog(
+                        [{
+                                type: 'address',
+                                name: 'user',
+                                indexed: true
+                            },
+                            {
+                                type: 'uint256',
+                                name: 'amount'
+                            }
+                        ],
+                        result.data,
+                        result.topics.slice(1)
+                    );
+                    console.log(`Unstake from ${data.user}: ${data.amount} tokens.\n`);
+                } else if (result.topics[0] === filter.topics[3]) {
+                    console.log('Withdraw Log:', result);
 
-                // Unstake event
-                const data = web3.eth.abi.decodeLog(
-                    [{
-                            type: 'address',
-                            name: 'user',
-                            indexed: true
-                        },
-                        {
-                            type: 'uint256',
-                            name: 'amount'
-                        }
-                    ],
-                    result.data,
-                    result.topics.slice(1)
-                );
-                console.log(`Unstake from ${data.user}: ${data.amount} tokens`);
-            } else if (result.topics[0] === filter.topics[3]) {
-                console.log('Result:', result);
-
-                // Withdraw event
-                const data = web3.eth.abi.decodeLog(
-                    [{
-                            type: 'address',
-                            name: 'token',
-                            indexed: true
-                        },
-                        {
-                            type: 'address',
-                            name: 'user',
-                            indexed: true
-                        },
-                        {
-                            type: 'uint256',
-                            name: 'amount'
-                        }
-                    ],
-                    result.data,
-                    result.topics.slice(1)
-                );
-                console.log(`Withdraw from ${data.user}: ${data.amount} tokens of ${data.token}`);
+                    // Withdraw event
+                    const data = web3.eth.abi.decodeLog(
+                        [{
+                                type: 'address',
+                                name: 'token',
+                                indexed: true
+                            },
+                            {
+                                type: 'address',
+                                name: 'user',
+                                indexed: true
+                            },
+                            {
+                                type: 'uint256',
+                                name: 'amount'
+                            }
+                        ],
+                        result.data,
+                        result.topics.slice(1)
+                    );
+                    console.log(`Withdraw from ${data.user}: ${data.amount} tokens of ${data.token}.\n`);
+                }
+            } catch (err) {
+                console.error('Unable to decode log.\n');
             }
         }
     }
